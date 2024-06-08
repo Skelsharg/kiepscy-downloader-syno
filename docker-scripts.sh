4fbf7a93f2bcdbaab55504a2ce7c53a1d24f7df7
Skelsharg <51878578+Skelsharg@users.noreply.github.com>
2024-06-08 22:14:49 +0200

    Update docker-scripts.sh

--- docker-scripts.sh
+++ docker-scripts.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 apt-get update
+apt-get upgrade -y
+apt-get install sudo -y
+apt-get install wget -y
+sudo wget -qO /usr/bin/yt-dlp https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp
+sudo chmod a+rx /usr/bin/yt-dlp
+sudo yt-dlp -U
+apt-get install python3 -y
 apt-get install -y --no-install-recommends git python3-pip python3-venv curl
 curl -fsSL https://deb.nodesource.com/setup_18.x | bash -
 apt-get install -y nodejs software-properties-common
